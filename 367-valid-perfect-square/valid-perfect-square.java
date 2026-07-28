e5b0c4208a098c9cbccfde87e804f2b31ac9c705
Gayatri koli <koligayatri59@gmail.com>
2026-07-28 22:43:29 +0530

    Time: 1 ms (21.35%) | Memory: 42.1 MB (58.16%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long i = 1;
+
+        while (i * i < num) {
+            i++;
+        }
+
+        return i * i == num;
+    }
+}
